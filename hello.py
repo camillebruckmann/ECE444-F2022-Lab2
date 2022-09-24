ef74af5c6c5112f4ec9073e8aaee43e332866427
Camille Bruckmann <camille@bruckmann.com>
2022-09-24 18:39:08 -0400

    Code for activity 3

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask import Flask, render_template
+from datetime import date, datetime
+from flask_moment import Moment
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello {}!</h1>'.format(name)
+    print(datetime.utcnow())
+    return render_template('user.html',name=name, current_time = datetime.utcnow())
